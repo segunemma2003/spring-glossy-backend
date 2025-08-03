0c6f79257276f3f714c01ccda5f4dd9f75c020be
kutteai <kutteai978@gmail.com>
2025-08-04 00:06:05 +0100

    a

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->withProviders([
-        App\Providers\RedisServiceProvider::class,
-    ])
     ->create();
--- config/database.php
+++ config/database.php
@@ -158,11 +158,6 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
-            'scheme' => env('REDIS_SCHEME', 'tcp'),
-            'ssl' => [
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-            ],
         ],
 
         'cache' => [
@@ -172,11 +167,6 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
-            'scheme' => env('REDIS_SCHEME', 'tcp'),
-            'ssl' => [
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-            ],
         ],
 
     ],
