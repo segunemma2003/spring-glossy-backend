ff91d0f3b7185e4f15addad835ee27ef860e9324
kutteai <kutteai978@gmail.com>
2025-08-05 06:14:06 +0100

    Fix root route - simplify for testing

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Laravel is working! Root route is accessible.';
 });
 
 Route::get('/test', function () {
     return 'Hello World! Laravel is working.';
+});
+
+Route::get('/simple', function () {
+    return 'Simple route test - Laravel is working!';
 });
