47e005275c8863ef6b2b28bbf78785d73d2db931
kutteai <kutteai978@gmail.com>
2025-08-10 12:41:46 +0100

    a

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -14,7 +14,7 @@ class AdminUserSeeder extends Seeder
             'first_name' => 'Admin',
             'last_name' => 'User',
             'email' => 'admin@springglossy.com.ng',
-            'password' => Hash::make('password'),
+            'password' => Hash::make('Nigeria@2025'),
             'email_verified_at' => now(),
             'is_admin' => true,
         ]);
